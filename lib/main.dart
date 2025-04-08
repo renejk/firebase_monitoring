9d39015c06eec850fc32251fbe10fde143af8860
rene.cardona <rene.cardona@pragma.com.co>
2025-04-08 18:38:40 -0500

    test send request to monitoring

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
+import 'package:http/http.dart' as http;
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -10,8 +11,23 @@ Future<void> main() async {
   runApp(const MainApp());
 }
 
-class MainApp extends StatelessWidget {
+class MainApp extends StatefulWidget {
   const MainApp({super.key});
+
+  @override
+  State<MainApp> createState() => _MainAppState();
+}
+
+class _MainAppState extends State<MainApp> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    final url = Uri.parse('https://fakestoreapi.com/products/1');
+    http.get(url).then((response) {
+      print(response.body);
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
